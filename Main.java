6d939c165c58a2ef31783fdf7b1c2193a620bfe6
Pravi Shukla <95701736+pravishukla91@users.noreply.github.com>
2025-01-08 17:23:27 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String name = "Pravi";
+        int age = 21;
+
+        System.out.println(name);
+        System.out.println(age);
+
+    }
+}
