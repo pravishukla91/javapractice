587588a373f5bdf18e5f8f4eea56b412def9beb0
Pravi Shukla <95701736+pravishukla91@users.noreply.github.com>
2025-01-08 22:38:46 +0530

    Add files via upload

---
+++ Arithemetics.java
@@ -0,0 +1,17 @@
+public class Arithemetics {
+    public static void main(String[] args){
+
+        int x = 10;
+        int y = 12;
+
+        int z;
+        //z = x+y;
+        //z = x - y;
+        //z = x * y;
+        //z = x / y;
+        z = x % y;
+
+        System.out.println(z);
+    }
+
+}
