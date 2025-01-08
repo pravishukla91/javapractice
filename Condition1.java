deb1f951d0a5c207202334d9be768658f06a1ea4
Pravi Shukla <95701736+pravishukla91@users.noreply.github.com>
2025-01-08 21:43:52 +0530

    Add files via upload

---
+++ Condition1.java
@@ -0,0 +1,10 @@
+public class Condition1 {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Adult");
+        } else {
+            System.out.println("Teenager");
+        }
+    }
+}
