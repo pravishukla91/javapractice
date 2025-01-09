950762a95d7fca006bee7225afaa30cea5556474
Pravi Shukla <95701736+pravishukla91@users.noreply.github.com>
2025-01-09 13:09:01 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,15 @@
+public class Constructor {
+
+        String name;
+        int age;
+        double weight;
+
+        Constructor(String name, int age, double weight){
+
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+
+}
+
---
+++ Constructor1.java
@@ -0,0 +1,9 @@
+public class Constructor1 {
+    public static void main(String[] args){
+
+        Constructor constructor2 = new Constructor("Rick",21,40);
+        Constructor constructor3 = new Constructor("jack", 22, 60);
+
+        System.out.println(constructor3.name);
+    }
+}
